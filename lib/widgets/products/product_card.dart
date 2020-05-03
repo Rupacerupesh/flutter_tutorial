e3c7ec9fba56b2f778298ceeb608e780baf464f9
Firelord rupace <firelordgunner@gmail.com>
2020-05-03 13:13:18 +0545

    url launcher

--- lib/widgets/products/product_card.dart
+++ lib/widgets/products/product_card.dart
@@ -41,8 +41,12 @@ class ProductCard extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.info),
             color: Theme.of(context).accentColor,
-            onPressed: () => Navigator.pushNamed<bool>(
-                context, '/product/' + model.allProducts[productIndex].id),
+            onPressed: () {
+              model.selectProduct(model.allProducts[productIndex].id);
+              Navigator.pushNamed<bool>(
+                      context, '/product/' + model.allProducts[productIndex].id)
+                  .then((_) => model.selectProduct(null));
+            },
           ),
           IconButton(
             icon: Icon(model.allProducts[productIndex].isFavorite
